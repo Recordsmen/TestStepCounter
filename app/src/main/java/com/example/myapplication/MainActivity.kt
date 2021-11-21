3d44b55d29663a224453e28deef45e7a9348678a
Recordsmen <uchitelev.alex@gmail.com>
2021-11-21 20:51:52 +0200

    New

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.example.myapplication
 
+import android.Manifest
+import android.app.Activity
+import android.app.Application
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
 import android.view.View
-import android.widget.Spinner
-import android.widget.ArrayAdapter
-import android.widget.TextView
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.material.snackbar.Snackbar
 import java.lang.Math.toDegrees
-import kotlin.math.PI
 
+const val TAG = "StepCounter"
+
+enum class FitActionRequestCode {
+    SUBSCRIBE
+}
 
 class MainActivity : AppCompatActivity() {
+    private val fitnessOptions = FitnessOptions.builder()
+        .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+        .build()
 
     private lateinit var spinner_fps: Spinner
     private lateinit var start: Button
@@ -28,6 +43,9 @@ class MainActivity : AppCompatActivity() {
     private var floatGeoMagnetic = FloatArray(3)
     private var floatOrientation = FloatArray(3)
     private var floatRotationMatrix = FloatArray(9)
+
+    private val runningQOrLater =
+        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -106,10 +124,17 @@ class MainActivity : AppCompatActivity() {
                 false -> true
                 true -> false
             }
+
             when (change){
                 true -> start.text = "Stop"
                 false -> start.text = "Start"
             }
+
+            when (change){
+                true -> steps.text = subscribe().toString()
+                false -> steps.text = "0"
+            }
+
             when (change) {
                 true -> {
                     val sensor_gyroscope = manager?.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
@@ -121,13 +146,13 @@ class MainActivity : AppCompatActivity() {
                     val check_magnetic = manager?.registerListener(listener, sensor_magnetic, SensorManager.SENSOR_DELAY_UI)
 
                     if (check_gyroscope == false) {
-                        gyroscope.text ="Fuck Niggas"
+                        gyroscope.text ="Gyroscope don't working"
                     }
                     if (check_accelerometer == false) {
-                        accelerometer.text ="Fuck Niggas"
+                        accelerometer.text ="Accelerometer don't working"
                     }
                     if (check_magnetic == false) {
-                        compass.text ="Fuck Niggas"
+                        compass.text ="Compass don't working"
                     }
                 }
                 false -> {
@@ -138,8 +163,91 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
+        checkPermissionsAndRun(FitActionRequestCode.SUBSCRIBE)
 
     }
+    private fun checkPermissionsAndRun(fitActionRequestCode: FitActionRequestCode) {
+        if (permissionApproved()) {
+            fitSignIn(fitActionRequestCode)
+        } else {
+            requestRuntimePermissions(fitActionRequestCode)
+        }
+    }
+
+    private fun fitSignIn(requestCode: FitActionRequestCode) {
+        if (oAuthPermissionsApproved()) {
+            performActionForRequestCode(requestCode)
+        } else {
+            requestCode.let {
+                GoogleSignIn.requestPermissions(
+                    this,
+                    requestCode.ordinal,
+                    getGoogleAccount(), fitnessOptions)
+            }
+        }
+    }
+
+    private fun performActionForRequestCode(requestCode: FitActionRequestCode) = when (requestCode) {
+        FitActionRequestCode.SUBSCRIBE -> subscribe()
+    }
+
+    private fun oAuthPermissionsApproved() = GoogleSignIn.hasPermissions(getGoogleAccount(), fitnessOptions)
+
+    private fun getGoogleAccount() = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+
+    private fun subscribe() {
+        Fitness.getRecordingClient(this, getGoogleAccount())
+            .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i(TAG, "Successfully subscribed!")
+                } else {
+                    Log.w(TAG, "There was a problem subscribing.", task.exception)
+                }
+            }
+    }
+    // не проходит проверку
+    private fun permissionApproved(): Boolean {
+        val approved = if (runningQOrLater) {
+            PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACTIVITY_RECOGNITION)
+        } else {
+            true
+        }
+        return approved
+    }
+
+    private fun requestRuntimePermissions(requestCode: FitActionRequestCode) {
+        
+        val shouldProvideRationale =
+            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)
+
+        requestCode.let {
+            if (shouldProvideRationale) {
+                Log.i(TAG, "Displaying permission rationale to provide additional context.")
+                Snackbar.make(
+                    findViewById(R.id.lay_main),
+                    R.string.permission_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok) {
+                        // Request permission
+                        ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                            requestCode.ordinal)
+                    }
+                    .show()
+            } else {
+                Log.i(TAG, "Requesting permission")
+                // Request permission. It's possible this can be auto answered if device policy
+                // sets the permission in a given state or the user denied the permission
+                // previously and checked "Never ask again".
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                    requestCode.ordinal)
+            }
+        }
+    }
+
+
 }
